cc709fde2119de4a40b0b87eaa228d826d3f0beb
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-11-17 23:12:17 -0300

    exmplo de lista de visualização da web

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -5,5 +5,9 @@ from app.models.tables import Pessoa
 @app.route('/')
 @app.route('/listagem')
 def listagem():
-    pessoas = ''   #Pessoa.query.all()
-    return render_template('listagem.html', pessoas=pessoas, ordem='id')
+    lista_pessoas = [
+        {'id': 1, 'nome' : 'Fulano de tal', 'idade': 18, 'sexo': 'M', 'salario':2000},
+        {'id': 2, 'nome': 'Beotrano da Silva', 'idade': 19, 'sexo': 'M', 'salario':2500},
+        {'id': 3, 'nome': 'Fulana de tal', 'idade': 17, 'sexo':'F', 'salario': 2100}
+    ]   #Pessoa.query.all()
+    return render_template('listagem.html', pessoas=lista_pessoas, ordem='id')
