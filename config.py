b16c1d4bf95e61bf3ecad638575191e40b140dba
Artur-Hugo <Artur.Hugoliga@gmail.com>
2021-11-22 15:47:42 -0300

    comitanto

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/db?host=localhost?port=3306"'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'banco.db')  
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/db?host=localhost?port=3306'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
   #'sqlite:///' + os.path.join(basedir, 'banco.db')
